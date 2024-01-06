3c2d9715776ecd82a86c85a347e7c3040ede3757
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2024-01-07 00:07:28 +0200

    Phone store v1.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     'react/prop-types': 0,
     'react/self-closing-comp': 0,
     'react/display-name': 0,
+    'react/react-in-jsx-scope': 0,
+    'jsx-a11y/label-has-associated-control': 0,
 
     // JavaScript
     semi: 0,
